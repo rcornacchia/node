7244f2dff09ee1adcff3509a4ebe71b4790a2e12
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-12-30 01:11:26 -0500

    nodes everywhere

--- meanMachineBook/awesome-test/server.js
+++ meanMachineBook/awesome-test/server.js
@@ -1,22 +1,60 @@
-// get the http and filesystem modules
-var http    = require('http');
-var fs      = require('fs');
+// load the express package and create our app
+var express = require('express');
+var app     = express();
+var path    = require('path');
 
-// create our server using the http module
-http.createServer(function(req, res) {
-    // write to our serer. set configuration for the response
-    res.writeHead(200, {
-        'Content-Type': 'text/html',
-        'Access-Control-Allow-Origin': '*'
-    });
+// send our index.html file to the user for the home page
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
-    // grab the index.html file using fs
-    var readStream = fs.createReadStream(__dirname + '/index.html');
+// get an instance of the router
+var adminRouter = express.Router();
 
-    // send the index.html file to our user
-    readStream.pipe(res);
+// route middleware that will happen on every request
+adminRouter.use(function(req, res, next) {
+    // log each request to the console
+    console.log(req.method, req.url);
 
-}).listen(1337);
+    // continue doing what we were doing and go to the route
+    next();
+});
 
-// tell ourselves what's happening
-console.log("Server started at http://localhost:1337");
+// admin main page. the dashboard (http://localhost:1337/admin)
+adminRouter.get('/', function(req, res) {
+    res.send('I am the dashboard!');
+});
+
+// users page (http://localhost:1337/admin/users)
+adminRouter.get('/users', function(req, res) {
+    res.send('I show all the users!');
+});
+
+// route middleware to validate :name
+adminRouter.param('name', function(req, res, next, name) {
+    // do validation on name here
+    // blah blah validation// log something so we know its working
+    console.log('doing name validations on ' + name);
+
+    // once validation is done save the new item in the req
+    req.name = name;
+    // go to the next thing
+    next();
+});
+
+// route with parameters (http://localhost:1337/admin/users/:name)
+adminRouter.get('/users/:name', function(req, res) {
+    res.send('hello ' + req.params.name + '!');
+});
+
+// posts page (http://localhost:1337/admin/posts)
+adminRouter.get('/posts', function(req, res) {
+    res.send('I show all the posts!');
+});
+
+// apply the routes to our application
+app.use('/admin', adminRouter);
+
+// start the server
+app.listen(1338);
+console.log('Server started on port 1338');
