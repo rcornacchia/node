80f7e2d91d5f79482e7cd2b44516b9bb9934c3d1
Rob Cornacchia <bobbycornacchia@gmail.com>
2016-01-06 15:30:52 -0500

    javascript additions

--- webAppDevBook/ch4/app.js
+++ webAppDevBook/ch4/app.js
@@ -1,6 +1,8 @@
 var main = function() {
     "use strict";
     $(".comment-input button").on("click", function(event) {
+        var $new_comment = $("<p>");
+        $new_comment.text("this is a new comment");
         $(".comments").append("<p>this is a new comment</p>");
     });
 };
