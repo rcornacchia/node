4a81f208697d1acf4248d67b4b6814e5c52696bd
Rob Cornacchia <bobbycornacchia@gmail.com>
2016-01-06 17:46:25 -0500

    textbox now clears automatically after entry

--- webAppDevBook/ch4/app.js
+++ webAppDevBook/ch4/app.js
@@ -1,10 +1,12 @@
 var main = function() {
     "use strict";
     $(".comment-input button").on("click", function(event) {
+        var $new_comment;
         if($(".comment-input input").val() !== "") {
             // create jquery object to hold input text
-            var $new_comment = $("<p>").text($(".comment-input input").val());
+            $new_comment = $("<p>").text($(".comment-input input").val());
             $(".comments").append($new_comment);
+            $(".comment-input input").val("");
         }
     });
 };
