7419bbd7f9fbc9db5f6ef3219683aa831e43a9c5
Rob Cornacchia <bobbycornacchia@gmail.com>
2016-01-06 18:00:05 -0500

    enter key now adds comment

--- webAppDevBook/ch4/app.js
+++ webAppDevBook/ch4/app.js
@@ -1,5 +1,7 @@
 var main = function() {
     "use strict";
+
+    // makes clicking button work
     $(".comment-input button").on("click", function(event) {
         var $new_comment;
         if($(".comment-input input").val() !== "") {
@@ -9,6 +11,20 @@ var main = function() {
             $(".comment-input input").val("");
         }
     });
+
+    // making enter work
+    $(".comment-input input").on("keypress", function(event) {
+        var $new_comment;
+
+        if(event.keyCode === 13) {
+            if ($(".comment-input input").val() !== "") {
+                $new_comment = $("<p>").text($(".comment-input input").val());
+                $(".comments").append($new_comment);
+                $(".comment-input input").val("");
+            }
+        }
+    });
+
 };
 
 $(document).ready(main);
