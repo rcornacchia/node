077b7c50f86c6711d33b231231d5d222bee689af
Rob Cornacchia <bobbycornacchia@gmail.com>
2016-01-06 16:58:57 -0500

    adding comments works

--- webAppDevBook/ch4/app.js
+++ webAppDevBook/ch4/app.js
@@ -2,8 +2,10 @@ var main = function() {
     "use strict";
     $(".comment-input button").on("click", function(event) {
         var $new_comment = $("<p>");
-        $new_comment.text("this is a new comment");
-        $(".comments").append("<p>this is a new comment</p>");
+        var comment_text = $(".comment-input input").val();
+        $new_comment.text(comment_text);
+        console.log(comment_text);
+        $(".comments").append($new_comment);
     });
 };
 
