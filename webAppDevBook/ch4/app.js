885529ed278bf9ed874872338b6d69756aeb2c26
Rob Cornacchia <bobbycornacchia@gmail.com>
2016-01-06 17:28:39 -0500

    fixed bug where comments could be null

--- webAppDevBook/ch4/app.js
+++ webAppDevBook/ch4/app.js
@@ -1,11 +1,11 @@
 var main = function() {
     "use strict";
     $(".comment-input button").on("click", function(event) {
-        var $new_comment = $("<p>");
-        var comment_text = $(".comment-input input").val();
-        $new_comment.text(comment_text);
-        console.log(comment_text);
-        $(".comments").append($new_comment);
+        if($(".comment-input input").val() !== "") {
+            // create jquery object to hold input text
+            var $new_comment = $("<p>").text($(".comment-input input").val());
+            $(".comments").append($new_comment);
+        }
     });
 };
 
