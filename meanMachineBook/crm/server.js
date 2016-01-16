622310f48af6434d82285c0be458c1651fb4f466
Rob Cornacchia <bobbycornacchia@gmail.com>
2016-01-16 12:32:38 -0500

    routing authentication

--- meanMachineBook/crm/server.js
+++ meanMachineBook/crm/server.js
@@ -1,15 +1,14 @@
-// Base Setup
-
-
 // Call the Packages
 var express     = require('express');       // call express
 var app         = express();                // define our app using express
 var bodyParser  = require('body-parser');   // get body parser
 var morgan      = require('morgan');        // used to see requests
-var mongoose    = require('mongoose');      // communicates w database
-mongoose.connect('mongodb://localhost/test');
 var port        = process.env.PORT||8080;   // set the port
 var User        = require('./app/models/user');
+var jwt         = require('jsonwebtoken');
+var superSecret = "ilovescotchscotchyscotchscotch";
+var mongoose    = require('mongoose');      // communicates w database
+mongoose.connect('mongodb://localhost/test');
 
 // App Configuration
 // use body parser so we can grab information from POST requests
@@ -36,13 +35,80 @@ app.get('/', function(req, res) {
 // get an instance of the express router
 var apiRouter = express.Router();
 
+// route for authenticating users (POST http://localhost:808/api/authenticate)
+apiRouter.post('/authenticate', function (req, res) {
+    // find the user
+    // select the name username and password explicitly
+    User.findOne({
+        username: req.body.username
+    }).select('name username password').exec(function(err, user) {
+        if (err) throw err;
+
+        // no user with that username was found
+        if(!user) {
+            res.json({
+                success: false,
+                message: 'Authentication failed. User not found.'
+            });
+        } else if (user) {
+            // check if password matches
+            var validPassword = user.comparePassword(req.body.password);
+            if(!validPassword) {
+                res.json({
+                    success: false,
+                    message: 'Authentication failed. Wrong password.'
+                });
+            } else {
+                // if user is found and password is right
+                // create a token
+                var token = jwt.sign({
+                    name: user.name,
+                    username: user.username
+                }, superSecret, {
+                    expiresInMinutes: 1440 // expires in 24 hours
+                });
+
+                // return the information including token as json
+                res.json({
+                    success: true,
+                    message: 'Enjoy your token!',
+                    token: token
+                });
+            }
+        }
+    });
+});
+
 apiRouter.use(function(req, res, next) {
     // do logging
     console.log('Somebody just came to our app!');
-    // we'll add more to the middleware in Chapter 10
-    // this is where we will authenticate users
 
-    next(); // make sure we go to the next routek
+    // check header or url parameters or post parameter for token
+    var token = req.body.token || req.param('token') || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+        // verifies secret and checks expiration
+        jwt.verify(token, superSecret, function(err, decoded) {
+            if(err) {
+                return res.status(403).send({
+                    success: false,
+                    message: 'Failed to authenticate token.'
+                });
+            } else {
+                // if everthing is good, save to request for use in other routes
+                req.decoded = decoded;
+                next();
+            }
+        });
+    } else {
+        // if there is no token
+        // return an HTTP response of 403 (access forbidden) and an error message)
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+    }
 });
 
 // test route to make sure everything is owkring
@@ -131,6 +197,10 @@ apiRouter.route('/users')
                 res.json({ message: 'Successfully deleted' });
             });
         });
+
+apiRouter.get('/me', function(req, res) {
+    res.send(req.decoded);
+});
 
 // more routes go here
 
